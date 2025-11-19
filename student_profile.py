1e0e8b89e771081fc7f7842aa64587bbbf5ed033
gprerana212-oss <gprerana212@gmail.com>
2025-11-19 11:48:25 +0530

    Update student_profile.py

--- student_profile.py
+++ student_profile.py
@@ -10,11 +10,11 @@ department = sys.argv[3]
 semester = sys.argv[4]
 
 if name and usn and department and semester:
-    print("\n========== STUDENT PROFILE CARD ==========")
+    print(" STUDENT PROFILE CARD ")
     print(f"Name       : {name}")
     print(f"USN        : {usn}")
     print(f"Department : {department}")
     print(f"Semester   : {semester}")
-    print("===========================================\n")
+    
 else:
     print("Error: All four fields must be provided!")
